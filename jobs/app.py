531971202774e66ea201869325a09c9333db77e8
Arunraj Nair <arunraj@msn.com>
2020-10-25 21:25:42 +0530

    Module 8 - Employer Reviews

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,8 @@
 import sqlite3
 
-from flask import Flask, g
+import datetime
+
+from flask import Flask, g, request, redirect, url_for
 from flask import render_template
 
 PATH = "db/jobs.sqlite"
@@ -66,6 +68,24 @@ def employer(employer_id):
                           'review.employer_id WHERE employer.id = ?', [employer_id])
 
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
+@app.route("/employer/<employer_id>/review", methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        status = request.form['status']
+        title = request.form['title']
+        rating = request.form['rating']
+
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+                    (review, rating, title, date, status, employer_id), commit=True)
+
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id)
 
 
 @app.teardown_appcontext
